e860526f1a3160ac939c0e28cb761dade368adbc
lequanghao <haolq2002@gmail.com>
2023-03-06 12:05:04 +0700

    update

--- main.js
+++ main.js
@@ -25,6 +25,28 @@ setInterval(function next() {
     imgElement.setAttribute('src',imgs[number]);
 }, 2500);
 
+
+
 function giftFunction() {
     confirm("Bấm dô tấm ảnh á")
 }
+
+Audio.prototype.play = (function(play) {
+    return function () {
+      var audio = this,
+          args = arguments,
+          promise = play.apply(audio, args);
+      if (promise !== undefined) {
+        promise.catch(_ => {
+          var el = document.createElement("button");
+          el.innerHTML = "Play";
+          el.addEventListener("click", function(){play.apply(audio, args);});
+          this.parentNode.insertBefore(el, this.nextSibling)
+        });
+      }
+    };
+    })(Audio.prototype.play);
+    
+    document.getElementById('MyAudioElement').play()
+
+
